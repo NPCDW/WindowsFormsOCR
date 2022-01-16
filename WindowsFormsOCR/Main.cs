3c8de70810c99db42ed8146aeaed57da45e72055
chenyongli <chenyongli0520@qq.com>
2022-01-16 17:33:00 +0800

    添加项目文件。

---
+++ WindowsFormsOCR/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsOCR
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Setting_Click(object sender, EventArgs e)
+        {
+            new Setting().Show();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
