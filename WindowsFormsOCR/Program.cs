519d13e1b885e58eb1a29b1447088caee0f5c9d1
NPCDW <chenyongli0520@gmail.com>
2022-01-23 22:40:00 +0800

    程序图标，只允许运行一个实例

--- WindowsFormsOCR/Program.cs
+++ WindowsFormsOCR/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,7 +18,30 @@ namespace WindowsFormsOCR
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            using (MainForm mainForm = new MainForm()) {
+            RunningInstance();
+        }
+
+        private static void RunningInstance()
+        {
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            foreach (Process process in processes)
+            {
+                if (process.Id == current.Id)
+                {
+                    continue;
+                }
+                if (Assembly.GetExecutingAssembly().Location.Replace("/", "\\") == current.MainModule.FileName)
+                {
+                    MessageBox.Show("已经有一个程序实例在运行了，请看右下角通知区域图标" + System.Environment.NewLine
+                        + "F2: 划词翻译" + System.Environment.NewLine
+                        + "F4: 截图文本识别" + System.Environment.NewLine
+                        + "Ctrl + F2: 截图翻译");
+                    return;
+                }
+            }
+            using (MainForm mainForm = new MainForm())
+            {
                 Application.Run();
             }
         }
